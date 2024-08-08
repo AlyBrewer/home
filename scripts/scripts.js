ebef9c302e790cb3697e416e4cba3dfc316bcba6
AlyBrewer <21brewera@gmail.com>
2024-08-08 14:12:17 -0400

    basic html and css added

---
+++ scripts/scripts.js
@@ -0,0 +1,4 @@
+// scripts/script.js
+document.getElementById('myButton').addEventListener('click', function() {
+    document.getElementById('message').textContent = 'Hello, you clicked the button!';
+});
