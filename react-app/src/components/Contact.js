ad420ee396911a9086c267e4b0a9449bdd993752
AlyBrewer <21brewera@gmail.com>
2024-08-08 19:25:19 -0400

    Added nonfunctional contact form

--- react-app/src/components/Contact.js
+++ react-app/src/components/Contact.js
@@ -1,11 +1,13 @@
 // src/components/Contact.js
 import React from 'react';
 import '../styles/App.css';
+import ContactForm from './ContactForm';
 
 const Contact = () => (
     <main class="main">
         <p>Contact me through this awesome form I made:</p>
-        <p>I'm lame and haven't finished it yet</p>
+        <p>I'm lame and haven't finished it yet, so it doesn't work.</p>
+        <ContactForm />
     </main>
 );
 
