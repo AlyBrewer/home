31c7c41ad005d51d47f8601f86cbe2082073534c
AlyBrewer <21brewera@gmail.com>
2024-09-02 16:19:02 -0400

    fixed social links

--- react-app/src/components/SocialLinks.js
+++ react-app/src/components/SocialLinks.js
@@ -6,10 +6,10 @@ import '../styles/App.css';
 const SocialLinks = () => {
     return (
       <div className="social-links">
-            <a href="https://www.linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/alybrewer" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-linkedin-in"></i>
             </a>
-            <a href="https://github.com/yourprofile" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/alybrewer" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-github"></i>
             </a>
       </div>
